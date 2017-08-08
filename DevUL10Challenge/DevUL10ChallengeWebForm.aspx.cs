199033bf49fa64d4d6fa284fb6a61f77d10fa6ac
oliver fu <ofu997@gmail.com>
2017-08-08 03:18:13 -0700

    start dart class

---
+++ DevUL10Challenge/DevUL10ChallengeWebForm.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DevUL10Challenge
+{
+	public partial class DevUL10ChallengeWebForm : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			Dart ourDart = new Dart();
+			ourDart.Throw();
+			resultLabel.Text += ourDart.dartValue.ToString(); 
+		}
+			public class Dart
+			{
+
+			public int dartValue { get; set; }
+
+				Random randomDart = new Random();
+				public int Throw()
+				{
+					dartValue = randomDart.Next(20);
+					return dartValue;
+				}
+
+			}
+	}
+}
